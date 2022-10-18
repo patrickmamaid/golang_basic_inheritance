f8dcacd458abfd3a21c3eb68f63cfbc26ad3e083
Patrick Mamaid <patrickmamaid@gmail.com>
2022-10-18 12:03:44 -0700

    Basic inheritance

---
+++ main.go
@@ -0,0 +1,51 @@
+package main
+
+import "fmt"
+
+type AliveThing interface {
+	IsDead() bool
+	Age() int
+}
+type ImmortalCat struct {
+	age int
+}
+
+func (ic ImmortalCat) IsDead() bool {
+	return true
+}
+
+func (ic ImmortalCat) Age() int {
+	return ic.age
+}
+
+type Animal interface {
+	Lick() error
+}
+
+type Cat struct {
+	AliveThing
+}
+
+func (c Cat) Age() int {
+	return 3
+}
+
+func (c Cat) Lick() error {
+	fmt.Println("cat licked")
+
+	return nil
+}
+
+func main() {
+	ic := ImmortalCat{
+		age: 999999,
+	}
+	c := Cat{
+		ic,
+	}
+	c.Lick()
+	fmt.Printf("Cat is alive: %v\n", c.IsDead())
+	fmt.Printf("cat's age: %d\n", c.Age())
+}
+
+
